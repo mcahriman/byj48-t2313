41861913a3b70f5a31e959e375bf4702ad3fdc14
Yurii Borysov <mcahriman@gmail.com>
2018-01-22 00:15:57 +0200

    Made that byj move on t2313
    still wondering if different speeds are achivable on 5v with uln2003.
    1V voltage drop makes byj24 even more anemic, so stable function in both
    directions with observable torque was only possible with 1ms delay
    between steps

--- src/application.c
+++ src/application.c
@@ -4,10 +4,52 @@
 #include "framework/byj48.h"
 #include "framework/millis.h"
 
-void init() {
+#include <util/delay.h>
 
+
+#define BTN_COOLDOWN 160;
+
+
+void init() {
+	DDRB |= 0x0F;
+	DDRD |= _BV(PD4) | _BV(PD5);
+	PORTD |= (_BV(DDD2) | _BV(DDD3));
 }
 
 void loop() {
+	static int current_step;
+	static uint8_t direction;
+	static uint8_t cooldown_timer;
+	static uint8_t motors_on;
+
+
+	_delay_ms(1)   ;// debug mode 120 Hz (meh)
+
+	if(cooldown_timer != 0) cooldown_timer--;
+
+	if( bit_is_clear(PIND, PD3) && cooldown_timer == 0) {
+		direction = !direction;
+		cooldown_timer = BTN_COOLDOWN;
+	}
+
+	if( bit_is_clear(PIND, PD2) && cooldown_timer == 0) {
+		motors_on = !motors_on;
+		cooldown_timer = BTN_COOLDOWN;
+	}
+
+
+	PORTB &= ~ 0x0F;
+	PORTD &= ~( _BV(PD5) |_BV(PD4)) ;
+
+	if(motors_on) {
+		PORTB |= byj_steps[current_step];
+		current_step += (direction == 0 ? 1 : -1);
+	}
+
+	PORTD |= (direction ? _BV(PD5) : 0) | (motors_on ? 0 : _BV(PD4) );
+
+
+	current_step = current_step < 8 ? current_step : 0;
+	current_step = current_step < 0 ? 7 : current_step;
 
 }
